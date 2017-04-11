f11ccb32d9f21bfff2730fe6725049f2e7bcbe8b
Klaas Govaerts <govaerts.klaas@gmail.com>
2017-04-11 15:19:55 +0200

    Codevoorbeelden les toegevoegd

---
+++ src/texteditor/StackEmptyException.java
@@ -0,0 +1,10 @@
+package texteditor;
+
+public class StackEmptyException extends RuntimeException {
+
+	/**
+	 * @author P. Cordemans
+	 */
+	private static final long serialVersionUID = -3099329785191944596L;
+
+}
